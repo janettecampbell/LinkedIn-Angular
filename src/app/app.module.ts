c238e12fecabf278c019571a9b444d9fced005b3
Janette Campbell <campbjp@hotmail.com>
2022-08-02 11:00:24 -0400

    feature: if no watchedOn provided it will not show

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MediaItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
