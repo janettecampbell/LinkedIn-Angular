6dab05f200c6a86e7651a4d8e64528a587008e65
Janette Campbell <campbjp@hotmail.com>
2022-08-02 20:00:59 -0400

    feature: added search form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  firstMediaItem = {
-    id: 1,
-    name: 'Firebug',
-    medium: 'Series',
-    category: 'Science Fiction',
-    year: 2010,
-    watchedOn: null,
-    isFavorite: false,
-  };
-
-  onMediaItemDelete(mediaItem) {}
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
-  declarations: [AppComponent, MediaItemComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
